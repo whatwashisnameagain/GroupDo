a80b1bbf227743a2b6c196785b2652f543f2b1c4
Reuben Remy <reubenremy@gmail.com>
2024-02-27 22:31:29 -0500

    chore: configure daisy ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
-      <div>Hello World</div>
-      <Link href="/users">Go to Users</Link>
+      <ProductCard/>
     </main>
   );
 }
--- app/users/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NewUserPage() {
-  return (
-    <div>NewUserPage</div>
-  )
-}
-
-export default NewUserPage
